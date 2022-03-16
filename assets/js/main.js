327a15c52fff72ef7df1e46da7582fad939edf41
AngrySquirrell <guillaume.maugin@efrei.net>
2022-03-16 14:00:19 +0100

    Removed scrollbar, added js

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const for_you = document.getElementById('for_you')
+
+
+function goRight (container) {
+    document.getElementById(container).scrollLeft += 304;
+};
+function goLeft (container) {
+    document.getElementById(container).scrollLeft -= 304;
+};
