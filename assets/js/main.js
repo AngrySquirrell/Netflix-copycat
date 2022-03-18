0cb8f82e6730b81cf016f8077ffbeaae072b37f8
AngrySquirrell <guillaume.maugin@efrei.net>
2022-03-18 18:06:55 +0100

    Added randomized images

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,64 +7,152 @@ function goLeft (container) {
     document.getElementById(container).scrollLeft -= offset;
 };
 
-const images_for_you = [
-    './assets/Series/Aggretsuko.jpg',
-    './assets/Series/AngelBeast.jpg',
-    './assets/Series/Arcane.jpg',
-    './assets/Series/BreakingBad.jpg',
-    './assets/Series/BlackMirror.jpg',
-    './assets/Series/BluePeriod.jpg',
-    './assets/Series/Django.png',
-    './assets/Series/GreatPretender.jpg',
-    './assets/Series/Hilda.jpg',
-    './assets/Series/JeVeuxMangerTonPancreas.jpg',
-    './assets/Series/LaLigneVerte.jpg',
-    './assets/Series/LeCupheadShow.jpg',
-    './assets/Series/LesEnfantsDeLaBalaine.jpg',
-    './assets/Series/LeVoyageTeChihiro.jpg',
-    './assets/Series/QuoiQuilArriveJeVousAime.jpg',
-    './assets/Series/NosMotsCommeDesBulles.jpg',
-    './assets/Series/SkyHighSurvival.jpg',
-    './assets/Series/toradora.jpg',
-    './assets/Series/SkyHighSurvival.jpg',
-    './assets/Series/Viking.jpg']; // 20
-const images_japanese_animation = [];
-const images_netflix_originals = [];
-const images_films_sf = [];
-const images_studios_ghibli = [];
-const images_dessins_animes = [];
 
-function random_array(iterations){
-    var randomArray = [];
-    for (let i = 0; i<iterations; i++){
-        randomArray.push(Math.floor(Math.random()*iterations))
+window.onload = function () { 
+
+        //FOR YOU
+
+    console.log(Math.floor(Math.random() * 10));
+    var images = [
+        './assets/Series/Aggretsuko.jpg',
+        './assets/Series/AngelBeast.jpg',
+        './assets/Series/Arcane.jpg',
+        './assets/Series/BreakingBad.jpg',
+        './assets/Series/BlackMirror.jpg',
+        './assets/Series/BluePeriod.jpg',
+        './assets/Series/Django.png',
+        './assets/Series/GreatPretender.jpg',
+        './assets/Series/Hilda.jpg',
+        './assets/Series/JeVeuxMangerTonPancreas.jpg',
+        './assets/Series/LaLigneVerte.jpg',
+        './assets/Series/LeCupheadShow.jpg',
+        './assets/Series/LesEnfantsDeLaBalaine.jpg',
+        './assets/Series/LeVoyageTeChihiro.jpg',
+        './assets/Series/QuoiQuilArriveJeVousAime.jpg',
+        './assets/Series/NosMotsCommeDesBulles.jpg',
+        './assets/Series/SkyHighSurvival.jpg',
+        './assets/Series/toradora.jpg',
+        './assets/Series/SkyHighSurvival.jpg',
+        './assets/Series/Viking.jpg'
+    ];
+  var i = 0;
+    var MyList = document.getElementById("for_you")
+    while (i < 10) {
+      i++;
+      MyList.innerHTML += ('afterend', '<div><div class="w-72 "><a href="#"><img src="' + images[Math.floor(Math.random() * images.length)] + '" class="w-72 rounded"></a></div></div>')
     }
-    return randomArray
-}
 
-function attribute_for_you(number_of_images){
-    let randArr = random_array(number_of_images)
-    let index_for_you = [
-        'for_you_1',
-        'for_you_2',
-        'for_you_3',
-        'for_you_4',
-        'for_you_5',
-        'for_you_6',
-        'for_you_7',
-        'for_you_8',
-        'for_you_9',
-        'for_you_10',
-        'for_you_11',
-        'for_you_12',
-        'for_you_13',
-        'for_you_14'
-    ]
-    
-    for (let i = 0; i < number_of_images; i++){
-        console.log(images_for_you[randArr[i]])
-        console.log(randArr[i])
-        document.getElementsById(index_for_you[i]).src = images_for_you[randArr[i]]
+        //JAPANESE ANIMATION
+
+    console.log(Math.floor(Math.random() * 10));
+    var images = [
+        './assets/Series/GreatPretender.jpg',
+        './assets/Series/BNA.jpg',
+        './assets/Series/JeVeuxMangerTonPancreas.jpg',
+        './assets/Series/LaCollineAuxCoquelicots.jpg',
+        './assets/Series/NGNL.jpg',
+        './assets/Series/LeVoyageTeChihiro.jpg',
+        './assets/Series/SheRa.jpg',
+        './assets/Series/Radiant.jpg',
+        './assets/Series/toradora.jpg',
+        './assets/Series/YourName.jpg',
+        './assets/Series/VioletEvergarden.jpg',
+        './assets/Series/LesEnfantsDeLaBalaine.jpg',
+        './assets/Series/LoinDeToiPresDeMoi.jpg',
+        './assets/Series/PrincessMononoke.jpg',
+        './assets/Series/Baki.jpg',
+        './assets/Series/Aggretsuko.jpg',
+        './assets/Series/MonVoisinTotoro.jpg',
+        './assets/Series/NosMotsCommeDesBulles.jpg',
+        './assets/Series/PrincessMononoke.jpg',
+        './assets/Series/SouvenirsDemarnie.jpg'
+    ];
+  i = 0;
+    var MyList = document.getElementById("japanese_animation")
+    while (i < 10) {
+      i++;
+      MyList.innerHTML += ('afterend', '<div><div class="w-72 "><a href="#"><img src="'+ images[Math.floor(Math.random() * images.length)] + '" class="w-72 rounded"></a></div></div>')
+    }
+
+        //FILMS DE SF
+
+    console.log(Math.floor(Math.random() * 10));
+    var images = [
+            './assets/Series/BlackSummer.jpg',
+            './assets/Series/LeChateauAmbulant.jpg',
+            './assets/Series/MonVoisinTotoro.jpg',
+            './assets/Series/NGNL.jpg',
+            './assets/Series/Radiant.jpg',
+            './assets/Series/Rick&Morty.jpg',
+            './assets/Series/SheRa.jpg',
+            './assets/Series/SquidGame.jpg',
+            './assets/Series/WarDoggo.jpg',
+            './assets/Series/BlackMirror.jpg',
+            './assets/Series/BreakingBad.jpg',
+            './assets/Series/Braves.jpg',
+            './assets/Series/BNA.jpg'
+    ];
+    i= 0;
+    var MyList = document.getElementById("films_sf")
+    while (i < 10) {
+        i++;
+        MyList.innerHTML += ('afterend', '<div><div class="w-72"><a href="#"><img src="' + images[Math.floor(Math.random() * images.length)] + '" class="w-72 rounded"></a></div></div>')
     } 
-}
-attribute_for_you(10)
+
+        //STUDIOS  GHIBLI
+
+    console.log(Math.floor(Math.random() * 10));
+    var images = [
+        './assets/Series/LaCollineAuxCoquelicots.jpg',
+        './assets/Series/LeChateauAmbulant.jpg',
+        './assets/Series/LeVoyageTeChihiro.jpg',
+        './assets/Series/MonVoisinTotoro.jpg',
+        './assets/Series/PonyoSurLaFalaise.jpg',
+        './assets/Series/SouvenirsDemarnie.jpg',
+        './assets/Series/PrincessMononoke.jpg',
+        './assets/Series/Arrietty.jpg',
+        './assets/Series/LeRoyaumedesChats.jpg',
+        './assets/Series/LeVentSeLeve.jpg',
+        './assets/Series/MaryetlaFleurdelaSorciere.png',
+        './assets/Series/NausicaadelaValleeduVent.jpg',
+        './assets/Series/PorcoRosso.jpg',
+        './assets/Series/SitutendlOreille.jpg'
+        ];
+      i= 0;
+        var MyList = document.getElementById("studios_ghibli")
+        while (i < 10) {
+          i++;
+          MyList.innerHTML += ('afterend', '<div><div class="w-72"><a href="#"><img src="' + images[Math.floor(Math.random() * images.length)] + '" class="w-72 rounded"></a></div></div>')
+        }
+
+        //Dessins Animes
+
+    console.log(Math.floor(Math.random() * 10));
+    var images = [
+        './assets/Series/Arcane.jpg',
+        './assets/Series/Aggretsuko.jpg',
+        './assets/Series/AngelBeast.jpg',
+        './assets/Series/Baki.jpg',
+        './assets/Series/BluePeriod.jpg',
+        './assets/Series/GreatPretender.jpg',
+        './assets/Series/Hilda.jpg',
+        './assets/Series/jojo.jpg',
+        './assets/Series/LeCupheadShow.jpg',
+        './assets/Series/LegoNinjago.jpg',
+        './assets/Series/YourName.jpg',
+        './assets/Series/NGNL.jpg',
+        './assets/Series/jojo.jpg',
+        './assets/Series/AstérixetObélix.jpg',
+        './assets/Series/FamilleLoud.jpg',
+        './assets/Series/CodeLyoko.jpg',
+        './assets/Series/Garfield.jpg',
+        './assets/Series/TotalySpies.jpg',
+        './assets/Series/VoyageVersLaLune.jpg'
+        ];
+      i= 0;
+        var MyList = document.getElementById("dessins_animes")
+        while (i < 10) {
+          i++;
+          MyList.innerHTML += ('afterend', '<div><div class="w-72"><a href="#"><img src="' + images[Math.floor(Math.random() * images.length)] + '" class="w-72 rounded"></a></div></div>')
+        }
+  }
